77af978bf2b4742a428e245498245a3e9efdf159
Meph1k <k9nneo4@gmail.com>
2019-11-18 19:15:10 +0100

    add camera

--- App.js
+++ App.js
@@ -1,121 +1,16 @@
-// @flow
-import React, { Component } from "react";
-import { NativeModules, StyleSheet, Text, View, Image, ScrollView, Button, Dimensions } from "react-native";
-type Props = {};
-type State = { message: string };
-const { OpenCV } = NativeModules;
-export default class App extends Component<Props, State> {
-  state = {
-    message: ""
-  };
+import React from 'react';
+import { Router, Stack, Scene } from 'react-native-router-flux';
 
-  getCanny = async () => {
-    this.setState({ message: 'loading...' })
+import Buttons from './screens/Buttons';
+import Camera from './screens/Camera';
 
-    try {
-      const message = await OpenCV.getCanny();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
+const App = () => (
+  <Router>
+    <Stack key="root" hideNavBar>
+      <Scene key="buttons" component={Buttons} hideNavBar initial />
+      <Scene key="camera" component={Camera} hideNavBar />
+    </Stack>
+  </Router>
+);
 
-  getThreshold = async () => {
-    this.setState({ message: 'loading...' })
-
-    try {
-      const message = await OpenCV.getThreshold();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
-
-  getLines = async () => {
-    this.setState({ message: 'loading...' })
-
-    try {
-      const message = await OpenCV.getLines();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
-
-  getContours = async () => {
-    this.setState({ message: 'loading...' })
-
-    try {
-      const message = await OpenCV.getContours();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
-
-  getSegments = async () => {
-    this.setState({ message: 'loading...' })
-
-    try {
-      const message = await OpenCV.getSegments();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
-
-  getRoundContours = async () => {
-    this.setState({ message: 'loading...' })
-
-    try {
-      const message = await OpenCV.getRoundContours();
-      this.setState({
-        message
-      });
-    } catch(e) {
-      alert(e);
-    }
-  }
-
-  render() {
-    const { message } = this.state;
-    return (
-      <ScrollView>
-        <Text style={styles.welcome}>Message from Native:</Text>
-        <Button title="Get canny image" onPress={this.getCanny} />
-        <Button title="Get basic threshold image" onPress={this.getThreshold} />
-        <Button title="Get lines" onPress={this.getLines} />
-        <Button title="Get contours" onPress={this.getContours} />
-        <Button title="Get segments" onPress={this.getSegments} />
-        <Button title="Get round contours" onPress={this.getRoundContours} />
-        <Image
-          source={{ uri: `data:image/png;base64,${message}` }}
-          style={{ width: Dimensions.get('window').width, height: 300 }}
-        />
-      </ScrollView>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
-});
+export default App;
---
+++ screens/Buttons.js
@@ -0,0 +1,84 @@
+// @flow
+import React, { Component } from "react";
+import { NativeModules, StyleSheet, Text, View, Image, ScrollView, Button, Dimensions } from "react-native";
+type Props = {};
+type State = { message: string };
+const { OpenCV } = NativeModules;
+import { Actions } from 'react-native-router-flux';
+
+export default class App extends Component<Props, State> {
+  state = {
+    message: ""
+  };
+
+  getFilter = (func) => () => {
+    this.setState({ message: 'loading...' })
+
+    Actions.push('camera', { applyFilter: async() => {
+        try {
+          const message = await func();
+          this.setState({
+            message
+          });
+        } catch(e) {
+          alert(e);
+        }
+    }})
+  }
+
+  getCanny = async () => {
+    return await OpenCV.getCanny()
+  }
+
+  getThreshold = async () => {
+    return await OpenCV.getThreshold();
+  }
+
+  getLines = async () => {
+    return await OpenCV.getLines();
+  }
+
+  getContours = async () => {
+    return await OpenCV.getContours();
+  }
+
+  getSegments = async () => {
+    return await OpenCV.getSegments();
+  }
+
+  getRoundContours = async () => {
+    return await OpenCV.getRoundContours();
+  }
+
+  render() {
+    const { message } = this.state;
+    return (
+      <ScrollView>
+        <Text style={styles.welcome}>Message from Native:</Text>
+        <Button title="Get canny image" onPress={this.getFilter(this.getCanny)} />
+        <Button title="Get basic threshold image" onPress={this.getFilter(this.getThreshold)} />
+        <Button title="Get lines" onPress={this.getFilter(this.getLines)} />
+        <Button title="Get contours" onPress={this.getFilter(this.getContours)} />
+        <Button title="Get segments" onPress={this.getFilter(this.getSegments)} />
+        <Button title="Get round contours" onPress={this.getFilter(this.getRoundContours)} />
+        <Image
+          source={{ uri: `data:image/png;base64,${message}` }}
+          style={{ width: Dimensions.get('window').width, height: 300 }}
+        />
+      </ScrollView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  }
+});
